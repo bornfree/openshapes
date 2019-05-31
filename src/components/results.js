f2fee25a18fd3ee507fc2dfb08cf8adf9170de76
bornfree <harsha.xg@gmail.com>
2019-05-31 18:48:48 +0530

    Show results with rounder borders

--- src/components/results.js
+++ src/components/results.js
@@ -14,7 +14,10 @@ class Results extends React.Component {
     render(){
         return(
             <div id="results" className="top-margin-20">
-                <p><b>Drawings</b></p>
+                {this.props.resultImages.length > 0?
+                    <p><b>Drawings</b></p>
+                    :null
+                }
                 <div className="row">
                 {this.props.resultImages.map((i) => 
                     <div key={i} className="col-md-2">
